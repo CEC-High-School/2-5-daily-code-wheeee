aedc565ace79ec27d243af3408e77cb921e60c19
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-02-05 12:55:46 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <math.h>
+using namespace std;
+
+
+int main() {
+	int input;
+	int answer = 0;
+	cout << "what is one side of a cube"; 
+		cin >> input ;
+        answer = input * input * input;
+		cout << answer << endl;
+		int baby;
+		int answer = 0;
+		cin >> input;
+		answer = 6 * input * 6 * input;
+		cout << answer << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
